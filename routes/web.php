f8942ec537f10c7ee77a706ebe40c55a96279e03
sambalmatah <yudiarinugroho14@gmail.com>
2025-12-23 11:53:58 +0700

    pengenalan blade templating engine

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
 });
 
 // membuat route baru ke view home
-Route::get('/home', function () {
-    return view('home');
+Route::get('/blog', function () {
+    return view('blog');
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', ['title' => 'About Page']);
 });
 
 Route::get('/contact', function () {
